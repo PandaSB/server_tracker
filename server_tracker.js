c485f246bf5669991db1d5fe304e5cdd9bd580e8
Stephane BARTHELEMY <stephane.barthelemy@arris.com>
2019-08-19 15:45:45 +0200

    first commit

--- server_tracker.js
+++ server_tracker.js
@@ -2,6 +2,12 @@ var express = require("express");
 var app = express();
 var port = 3000;
 
+let db = new sqlite3.Database('./db/data.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chinook database.');
+});
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -10,3 +16,9 @@ app.listen(port, () => {
   console.log("Server listening on port " + port);
 });
 
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
